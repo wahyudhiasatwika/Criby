97a68b634e126ac94b0b39a14ca4e8035bc9ad59
wikachu <wikabsd@gmail.com>
2024-06-16 17:29:35 +0700

    Changed Photo Member

--- Machine Learning/Landing Page/criby/src/contants/member.ts
+++ Machine Learning/Landing Page/criby/src/contants/member.ts
@@ -4,5 +4,5 @@ export const Member = [
     { id: 3, name: 'Irham Ahmad Rozan', image: '/image/member/ojan.jpeg', description: 'Mobile Development' },
     { id: 4, name: 'Kenny Cristi Nehe', image: '/image/member/kenny.jpg', description: 'Mobile Development' },
     { id: 5, name: 'Aulia Razak Akmal', image: '/image/member/ojak.jpeg', description: 'Cloud Computing' },
-    { id: 6, name: 'Tri Wahyudi', image: '/image/member/wahyu.jpeg', description: 'Cloud Computing' },
+    { id: 6, name: 'Tri Wahyudi', image: '/image/member/wahyu.jpg', description: 'Cloud Computing' },
 ];
