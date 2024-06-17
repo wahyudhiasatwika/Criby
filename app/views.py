a7d19793c6f732cfec77bba48e29ffd24458f79e
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2024-06-17 20:36:23 +0700

    fixing views

--- app/views.py
+++ app/views.py
@@ -8,19 +8,22 @@ from rest_framework import status
 from rest_framework.renderers import JSONRenderer
 from tensorflow.keras.models import load_model
 from django.conf import settings
+from shutil import copyfileobj
 
 from .serializer import FileUploadSerializer
 from .utils import *
 import tensorflow_hub as hub
+from pydub import AudioSegment
 
 
 # Create your views here.
 class PredictEndPoint(APIView):
     parser_classes = (MultiPartParser, FormParser)
-    model = load_model(os.path.join(settings.MEDIA_ROOT, 'model_v2.h5'))
+    model = load_model(os.path.join(settings.MEDIA_ROOT, 'model_v3.5.h5'))
     inceptionv3 = hub.KerasLayer('https://www.kaggle.com/models/google/inception-v3/TensorFlow2/feature-vector/2')
-    data_dict = {3: 'hungry', 1: 'burping', 2: 'discomfort', 0: 'belly_pain', 4: 'tired'}
-    renderer_classes = [JSONRenderer,]
+    data_dict = {3: 'lapar', 1: 'bersendawa atau sendawa', 2: 'tidak nyaman', 0: 'sakit perut', 4: 'lelah atau letih'}
+    renderer_classes = [JSONRenderer, ]
+    allowed_file_extension = ['mp3', 'wav', 'mp4', 'm4a']
 
     def get(self, *args, **kwargs):
         return Response()
@@ -29,9 +32,18 @@ class PredictEndPoint(APIView):
         file_serializer = FileUploadSerializer(data=self.request.data)
         if file_serializer.is_valid():
             uploaded_file = self.request.FILES['file']
-            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-                temp_file.write(uploaded_file.read())
-                temp_filename = temp_file.name
+            ext_name: str = uploaded_file.name.split('.')[-1]
+
+            if ext_name in self.allowed_file_extension:
+                audio = AudioSegment.from_file(uploaded_file)
+                with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as temp_file:
+                    audio.export(temp_file.name, format='wav')
+                    temp_filename = temp_file.name
+            else:
+                msg_list = [f".{x}" for x in self.allowed_file_extension]
+                msg = ' or '.join(msg_list)
+                return Response({'error': f"File format extension not supported! Only {msg} are supported!"},
+                                status=status.HTTP_403_FORBIDDEN)
 
             try:
                 data = load_data(temp_filename, self.inceptionv3)
@@ -39,6 +51,7 @@ class PredictEndPoint(APIView):
                 pred = self.model.predict(data)
                 prediction = np.argmax(pred)
                 confidence = np.max(pred)
+                confidence = 100.0 if confidence * 100 > 1.0 else confidence
                 pred_result = self.data_dict[prediction]
 
             finally:
@@ -47,14 +60,13 @@ class PredictEndPoint(APIView):
 
             save_dir = os.path.join(settings.MEDIA_ROOT, 'result', pred_result)
             if not os.path.exists(save_dir):
-                os.mkdirs(save_dir)
+                os.mkdir(save_dir)
+
             save_path = os.path.join(save_dir, uploaded_file.name)
             with open(save_path, 'wb+') as destination:
-                for x in uploaded_file.chunks():
-                    destination.write(x)
+                copyfileobj(uploaded_file, destination)
 
-
-            return Response(data={'predictions': pred_result, 'confidence': float(confidence)},
+            return Response(data={'predictions': pred_result, 'confidence': confidence},
                             status=status.HTTP_200_OK)
 
-        return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': file_serializer.errors['file']}, status=status.HTTP_400_BAD_REQUEST)
