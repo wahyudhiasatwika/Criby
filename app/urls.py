f452e90f5faf167f9fe46547bb662cb0420516d1
bintangx1902 <bintangpratomoprawirodirjo@gmail.com>
2024-06-17 20:50:15 +0700

    add main routes

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PredictEndPoint
+from .views import PredictEndPoint, goto_domain
 
 urlpatterns = [
+    path('', goto_domain, name='goto-domain'),
     path('predict', PredictEndPoint.as_view(), name='predict')
 ]
